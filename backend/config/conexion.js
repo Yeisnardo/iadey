3140a947f8cdca13e15b552e0c06735a6163e3af
Yeisnardo <Yeisnardo30@gmail.com>
2026-04-02 11:43:00 -0400

    cambio

--- backend/config/conexion.js
+++ backend/config/conexion.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 // Configuración del pool de conexiones
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'nombre_base_datos',
-  user: process.env.DB_USER || 'usuario',
-  password: process.env.DB_PASSWORD || 'contraseña',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   max: 20, // máximo de conexiones en el pool
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
